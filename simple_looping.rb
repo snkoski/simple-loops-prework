15b1479c91594213e722917606ce72657055c160
Shawn Koski <shawnnkoski@gmail.com>
2018-03-29 10:51:20 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,9 +16,10 @@ def loop_iterator(number_of_times)
     counter += 1
     break if counter >= number_of_times
   end
-  
-  
 end
+
+loop_iterator
+
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
